4f72b7d0db08f4430f7a18b8df9b1ef8ab518124
tpaquier <theo.paquier@ensae.fr>
2023-03-09 16:43:00 +0000

    Question 6 faite et ça marche, y'a plus qu'à faire marcher le bfs

--- delivery_network/graph.py
+++ delivery_network/graph.py
@@ -4,6 +4,9 @@ class Graph:
         self.graph = dict([(n, []) for n in nodes])
         self.nb_nodes = len(nodes)
         self.nb_edges = 0
+        self.list_of_neighbours = []
+        self.list_of_edges = []
+        self.max_power =0
     
 
     def __str__(self):
@@ -43,19 +46,21 @@ class Graph:
         for i in ancetres:
             infos = self.graph[i]
             for v in infos : 
-                if v[3]>power : 
+                if v[2]>power : 
                     return None
-                if v[3]<=power : 
+                if v[2]<=power : 
                     return ancetres
 
 
-    def dfs(self, node, visites=[], composantes=[]) :  
+    def dfs(self, node, visites=[], composantes=[], power=-1) :  
         visites.append(node) 
 #on prend un point
         composantes.append(node)
         for i in self.graph[node] : 
-            if i[0] not in visites :
-                self.dfs(i[0], visites, composantes)  
+            if i[0] not in visites and power!=-1:
+                continue
+                self.dfs(i[0], visites, composantes) 
+        return visites
                 
 #et on applique à nouveau la fonction pour qu elle visite tous les voisins des voisins etc...   
 
@@ -74,7 +79,7 @@ class Graph:
         queue=[]
         ancetres.append(beg)
         queue.append(beg)
-        while len(queue)>0 or dest in queue:
+        while len(queue)>0 or dest != queue[0]:
             n = queue.pop()
             for v in self.graph[n]:
                 if v[0] not in ancetres:
@@ -83,26 +88,47 @@ class Graph:
         return ancetres
 
 
+    def BS(self, liste, power):
+        haut = len(liste)-1
+        bas = 0
+        mid = 0
+        while bas <= haut:
+            mid = (haut+bas)//2
+            if liste[mid] < power:
+                bas = mid+1
+            elif liste[mid] > power:
+                haut = mid-1
+            elif liste[mid] == power:
+                return mid
+        return -1 #si on arrive la c est que l element etait po dans la liste
+
+             
+
 
     def power_nodes(self, node1, node2):
         liste=self.graph[node1]
         for i in liste :
-
-            if i[1] = node2 :
+            if i[1] == node2 :
                 power = i[3]
         return power
             
 
-
-    def min_power(self, src, end):
-        liste=self.bfs(src, end)
-        power=[]
-        for i in liste:
-            power.append(self.power_nodes(i,i+1))
-        petiot=self.minimum(power)
-        return petiot
-
-
+    def min_power(self, src, dest):
+        debut=0
+        fin=self.max_power
+        if dest not in self.dfs(src):
+            return None
+        while debut != end: 
+            mid = (debut+fin)//2
+            if dest not in self.dfs(src, mid):
+                debut=mid
+            else:
+                fin=mid
+            if fin-debut == 1:
+                fin=debut
+        minus=fin
+        print("la plus petiote puissance pour le chemin est : "+minus)
+        #print("le chemin qui marche bien est :"+self.get_path_with_power(src, dest, minus))
 
 
 
@@ -128,24 +154,51 @@ class Graph:
         '''
 
 def graph_from_file(filename):
+    """
+    Reads a text file and returns the graph as an object of the Graph class.
+    The file should have the following format: 
+        The first line of the file is 'n m'
+        The next m lines have 'node1 node2 power_min dist' or 'node1 node2 power_min' (if dist is missing, it will be set to 1 by default)
+        The nodes (node1, node2) should be named 1..n
+        All values are integers.
+    Parameters: 
+    -----------
+    filename: str
+        The name of the file
+    Outputs: 
+    -----------
+    G: Graph
+        An object of the class Graph with the graph from file_name.
+    """
+    #start = time.perf_counter()
     file = open(filename, 'r')
     dist=1
     #First line is read in order to properly intialize our graph
     line_1 = file.readline().split(' ')
-    new_graph = Graph([node for node in range(1,int(line_1[0])+1)])
-    new_graph.nb_edges = int(line_1[1].strip('\n'))
+    total_nodes = int(line_1[0])
+    nb_edges = int(line_1[1].strip('\n'))
+    new_graph = Graph([node for node in range(1,total_nodes+1)])
+    new_graph.nb_edges = nb_edges
+    new_graph.list_of_edges = [None]*nb_edges
     #Then, all lines are read to create a new edge for each line
     for line in file:
         list_line = line.split(' ')
+        start_node = int(list_line[0])
+        end_node = int(list_line[1])
+        power = int(list_line[2])
         if list_line == []:
             continue
         if len(list_line) == 4:
             #In the case where a distance is included
             dist = int(list_line[3])
-        new_graph.add_edge(int(list_line[0]), int(list_line[1]), int(list_line[2]),dist)
-    print(new_graph.graph)
+        new_graph.max_power = max(new_graph.max_power, power)
+        new_graph.add_edge(start_node, end_node, power, dist)
+    new_graph.list_of_neighbours = [list(zip(*new_graph.graph[node]))[0] for node in new_graph.nodes if new_graph.graph[node]!=[]]
+    #stop = time.perf_counter()
+    #print(stop-start)
     file.close()
     return new_graph
+
 
 
 
@@ -158,4 +211,3 @@ def minimum(self, liste):
         if i > min:
             min=min
     return min
-
